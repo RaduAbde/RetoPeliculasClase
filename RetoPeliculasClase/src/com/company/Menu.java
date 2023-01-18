aff9ec10a07791f6fb0d1400fd4bf75ee280112c
RaduAbde <raduabde@gmail.com>
2023-01-18 20:05:38 +0100

    Commit añadiendo Menu a clase Menu sin Metodos

--- RetoPeliculasClase/src/com/company/Menu.java
+++ RetoPeliculasClase/src/com/company/Menu.java
@@ -1,4 +1,48 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Menu {
+    public void MostrarMenu(){
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("0==> Ordenar Por Nombre");
+        System.out.println("1==> Ordenar Por Nombre Desc");
+        System.out.println("2==> Ordenar Por Nº Estrellas");
+        System.out.println("3==> Ordenar Por Nº Estrellas Desc");
+        System.out.println("4==> Ordenar Por Nº Sala");
+        System.out.println("5==> Ordenar Por Nº Sala Desc");
+        System.out.println("6==> Filtrar Por Peliculas Vistas");
+        System.out.println("7==> Filtrar Por Peliculas No Vistas");
+
+        switch (sc.nextInt()){
+            case 0:{
+
+            }
+            case 1:{
+
+            }
+            case 2:{
+
+            }
+            case 3:{
+
+            }
+            case 4:{
+
+            }
+            case 5:{
+
+            }
+            case 6:{
+
+            }
+            case 7:{
+
+            }
+
+        }
+
+    }
 }
